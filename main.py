52bbda226cb5399c5bdfb7973d5057eab99ed226
SamVL26 <168585575+SamVL26@users.noreply.github.com>
2024-05-03 13:00:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
+pandas
+openpyxl
+import pandas as pd
+from flash import flash
 
-app=Flask(__name__)
+app=Flask(_name_)
 
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+base=pd.read_excel("pokemones.xlsx")
 
-if __name__=="__main__":
-  app.run()
+@app.router("/")
+def Principal():
+  return "Esta es una API que te muestra Pokemones"
+
+@app.router("Por numero(<Numero)")
+def PorNumero(Numero):
+  Numero=int(Numero)
+  fila=base[base["Numero"]==Numero]
+  respuestas=f"El pokemon (Numero) es(fila.loc[:,'Nombre'])"
+  return respuesta
+
+@app.router("/Por_Tipo/<Tipo>")
+def PorTipo(Tipo):
+  resultados-base[base["Tipo"]==Tipo]
+  Resultados= str (resultados)
+  return resultados 
+
+@app.router("/Por_Ataque/Defensa/Velocidad>'")
+def PorAtaque(Ataque):
+  Peso1=float(Defensa)
+  Peso2=float(Velocidad) 
+  resultados=base[("base["Ataque]Defensa)&(base["Ataque"]>Velocidad)]
+  return resultados
+print(PorAtaque(30))
+
+app.run()
